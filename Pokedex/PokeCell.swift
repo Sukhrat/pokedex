5d197f15961d33c0dd1d3c53490c956d1bbef1d0
Sukhrat <sukhrat.arziyev@gmail.com>
2017-01-31 17:09:55 +0600

    Parsed from csv file, added background audio

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Sukhrat on 31.01.17.
+//  Copyright © 2017 Sukhrat. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required  init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+    
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,35 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Sukhrat on 30.01.17.
+//  Copyright © 2017 Sukhrat. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexID: Int!
+    
+    var name: String {
+        
+        return _name
+
+    }
+    
+    var pokedexID: Int {
+        
+        return _pokedexID
+        
+    }
+    
+    init(name: String, pokedexID: Int) {
+        
+        self._name = name
+        self._pokedexID = pokedexID
+        
+    }
+    
+}
